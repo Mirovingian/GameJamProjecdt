f4664af09c4487a09a6e7c887d08a46a733c714a
Vladislav Kucherenko <vladislav.a.kucherenko@gmail.com>
2025-06-12 22:54:43 +0300

    feat: create logic for enemy

--- Assets/Game/Scripts/Game/Gameplay/Characters/Enemy/EnemyController.cs
+++ Assets/Game/Scripts/Game/Gameplay/Characters/Enemy/EnemyController.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float detectionRadius = 5f;
+    [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private LayerMask obstacleLayer;
+    [SerializeField] private EnemyGunController gun;
 
-    // Update is called once per frame
+    [SerializeField] private float fireRate = 1f;
+
+    private Transform player;
+    private float nextFireTime;
+    private bool canSeePlayer;
     void Update()
     {
-        
+        FindPlayer();
+
+        if (canSeePlayer && Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + 1f / fireRate;
+        }
+    }
+
+    void FindPlayer()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRadius, playerLayer);
+
+        canSeePlayer = false;
+
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("Player"))
+            {
+                player = hit.transform;
+                Vector2 directionToPlayer = (player.position - transform.position).normalized;
+
+                RaycastHit2D hitObstacle = Physics2D.Raycast(
+                    transform.position,
+                    directionToPlayer,
+                    detectionRadius,
+                    obstacleLayer);
+
+                if (!hitObstacle)
+                {
+                    canSeePlayer = true;
+                    break;
+                }
+            }
+        }
+    }
+    void Shoot()
+    {
+        if (player == null) return;
+
+        gun.Shoot(player);
     }
 }
--- Assets/Game/Scripts/Game/Gameplay/Characters/Enemy/EnemyGunController.cs
+++ Assets/Game/Scripts/Game/Gameplay/Characters/Enemy/EnemyGunController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class EnemyGunController : MonoBehaviour
 {
-    public GameObject bullet;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject bulletPrefab;
+    public float speed;
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Shoot(Transform player)
     {
-        
+        Vector2 direction = (player.position - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        bullet.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        bullet.GetComponent<Rigidbody2D>().gravityScale = 0;
     }
 }
